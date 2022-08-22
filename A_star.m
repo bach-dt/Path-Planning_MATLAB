0f864c07e4a23d6962ba372f9387802d41a51772
bach-dt <bach_dt190103@gmail.com>
2022-08-22 16:45:47 +0700

    add Documents

--- A_star.m
+++ A_star.m
@@ -4,85 +4,77 @@ classdef A_star
         % 3: CLOSED set
         function [] = A_star_path(obj, map, start, goal, map_size)
             hold on;
-        % COST_MAP(point_x, point_y) = [G_COST, H_COST, F_COST, parent_x, parent_y]  
-            COST_MAP = zeros(map_size - 1, map_size - 1, 5);
-            COST_MAP(round(start(1)), round(start(2)), 1) = 0;
-            COST_MAP(round(start(1)), round(start(2)), 2) = obj.COST(start, goal);
-            COST_MAP(round(start(1)), round(start(2)), 3) = ...
-                COST_MAP(round(start(1)), round(start(2)), 1) + ...
-                COST_MAP(round(start(1)), round(start(2)), 2);
-            
-            OPEN = [start(1),start(2),COST_MAP(round(start(1)), round(start(2)), 3)];
             map(round(start(1)), round(start(2))) = 2;
             CLOSED = [];
-            CURRENT = start;
-            while (CURRENT(1) ~= goal(1) || CURRENT(2) ~= goal(2))
+            CURRENT.data = start;
+            CURRENT.g_cost = 0;
+            CURRENT.h_cost = obj.COST(start, goal);
+            CURRENT.f_cost = CURRENT.g_cost + CURRENT.h_cost;
+            CURRENT.parent = [];
+            OPEN = [];
+            OPEN = [OPEN, CURRENT];
+            
+            G_COST_MAP = zeros(map_size, map_size, 1);
+            G_COST_MAP(start(1) + 0.5, start(2) + 0.5, 1) = 0;
+            
+            while (CURRENT.data(1) ~= goal(1) || CURRENT.data(2) ~= goal(2))
                 min_f_cost = map_size * 2;
                 CURRENT_index = 0;
-                for index = 1: 1: length(OPEN)/3
-                    if OPEN( 3 * index ) < min_f_cost
-                        min_f_cost = OPEN( 3 * index );
-                        CURRENT = [OPEN(3 * index - 2), OPEN(3 * index - 1)];
-                        CURRENT_index = 3 * index - 2;
+                for index = 1: 1: length(OPEN)
+                    if OPEN(index).f_cost < min_f_cost
+                        min_f_cost = OPEN(index).f_cost;
+                        CURRENT = OPEN(index);
+                        CURRENT_index = index;
                     end
                 end
                 
-                disp('current: ');
-                disp(CURRENT);
-                disp('open: ');
-                disp(OPEN);
-                if (CURRENT(1) ~= start(1) || CURRENT(2) ~= start(2)) &&...
-                        (CURRENT(1) ~= goal(1) || CURRENT(2) ~= goal(2))
-                    plot(CURRENT(1), CURRENT(2),'whiteo',...
+            % plot CURRENT (unnecessary)
+                if (CURRENT.data(1) ~= start(1) || CURRENT.data(2) ~= start(2)) &&...
+                        (CURRENT.data(1) ~= goal(1) || CURRENT.data(2) ~= goal(2))
+                    plot(CURRENT.data(1), CURRENT.data(2),'whiteo',...
                         'LineWidth',1,...
                         'MarkerSize',round(360/ map_size),...
                         'MarkerEdgeColor',[0.9, 1, 0.9],...
                         'MarkerFaceColor',[0.9, 1, 0.9]);
                 end
 
-            % remove CURRENT from OPEN set
-                    OPEN(CURRENT_index) = [];
-                    OPEN(CURRENT_index) = [];
-                    OPEN(CURRENT_index) = [];
-            % add CURRENT to CLOSED set
+            % remove CURRENT.data from OPEN set
+                OPEN(CURRENT_index) = [];
+                    
+            % add CURRENT.data to CLOSED set
                 CLOSED = [CLOSED; CURRENT];
-                map(round(CURRENT(1)), round(CURRENT(2))) = 3; 
+                map(round(CURRENT.data(1)), round(CURRENT.data(2))) = 3; 
 
-            % continue
-                neighbor = [];
-                for neighbor_x = CURRENT(1)-1: 1: CURRENT(1)+1
-                    for neighbor_y = CURRENT(2)-1: 1: CURRENT(2)+1
+            % define neighbor and for each neighbor
+                for neighbor_x = CURRENT.data(1)-1: 1: CURRENT.data(1)+1
+                    for neighbor_y = CURRENT.data(2)-1: 1: CURRENT.data(2)+1
+                        
+                    % condition for array index
                         if (neighbor_x > 0) && (neighbor_y > 0)
-                            if (neighbor_x ~= CURRENT(1) || neighbor_y ~= CURRENT(2))...
-                                    && map(round(neighbor_x), round(CURRENT(2))) ~= 1 ...
-                                    && map(round(CURRENT(1)), round(neighbor_y)) ~= 1 
+                            if (neighbor_x ~= CURRENT.data(1) || neighbor_y ~= CURRENT.data(2))...
+                                    && check_valid(obj, map, CURRENT.data) == 1
                                 
-                                this_neighbor = [neighbor_x, neighbor_y];
-                                neighbor = [neighbor; this_neighbor];
-                        % for each neighbor
-                                if (map(round(neighbor_x), round(neighbor_y)) == 1 || map(round(neighbor_x), round(neighbor_y)) == 3)
+                                this_neighbor.data = [neighbor_x, neighbor_y];
+                                
+                            % if neighbor is CLOSED
+                                if (map(round(neighbor_x), round(neighbor_y)) == 3)
                                     continue;
                                 end
-                                neighbor_new_g_cost = obj.COST(this_neighbor, CURRENT) + ...
-                                                      COST_MAP(round(CURRENT(1)), round(CURRENT(2)), 1);
+                                
+                                neighbor_new_g_cost = obj.COST(this_neighbor.data, CURRENT.data) + ...
+                                                      CURRENT.g_cost;
 
-                                if (COST_MAP(round(this_neighbor(1)), round(this_neighbor(2)), 3) == 0 || ...
-                                    neighbor_new_g_cost < COST_MAP(round(this_neighbor(1)), round(this_neighbor(2)), 1))
-
-                                    COST_MAP(round(this_neighbor(1)), round(this_neighbor(2)), :) ...
-                                        = [neighbor_new_g_cost, obj.COST(this_neighbor, goal), ...
-                                        neighbor_new_g_cost + obj.COST(this_neighbor, goal), ...
-                                        CURRENT];
-
-
-                                    if (map(round(this_neighbor(1)), round(this_neighbor(2))) ~= 2)
-
-                                        disp('neighbor')
-                                        disp(this_neighbor);
-                                        map(round(this_neighbor(1)), round(this_neighbor(2))) = 2;
-                                        OPEN = [OPEN, this_neighbor(1), this_neighbor(2), neighbor_new_g_cost + obj.COST(this_neighbor, goal)];
+                                if G_COST_MAP(this_neighbor.data(1) + 0.5, this_neighbor.data(2) + 0.5, 1) == 0 ||...
+                                        neighbor_new_g_cost < G_COST_MAP(this_neighbor.data(1) + 0.5, this_neighbor.data(2) + 0.5, 1)
+                                    this_neighbor.g_cost = neighbor_new_g_cost;
+                                    this_neighbor.h_cost = obj.COST(this_neighbor.data, goal);
+                                    this_neighbor.f_cost = this_neighbor.g_cost + this_neighbor.h_cost;
+                                    this_neighbor.parent = CURRENT;
+                                    if map(round(this_neighbor.data(1)), round(this_neighbor.data(2))) ~= 2
+                                        G_COST_MAP(this_neighbor.data(1) + 0.5, this_neighbor.data(2) + 0.5, 1) = neighbor_new_g_cost;
+                                        map(round(this_neighbor.data(1)), round(this_neighbor.data(2))) = 2;
+                                        OPEN = [OPEN, this_neighbor];
                                     end
-
                                 end
 
                             end
@@ -94,11 +86,9 @@ classdef A_star
         % print path
             path = [];
             point = CURRENT;
-            while (point(1) ~= start(1) || point(2) ~= start(2))
-                point = [COST_MAP(round(point(1)), round(point(2)), 4),...
-                    COST_MAP(round(point(1)), round(point(2)), 5)];
-                path = [path; point];
-                disp(point);
+            while (point.data(1) ~= start(1) || point.data(2) ~= start(2))
+                point = point.parent;
+                path = [path; point.data];
             end
             
             path(length(path), :) = [];
@@ -116,6 +106,18 @@ classdef A_star
             cost = + min(abs(A(1) - B(1)), abs(A(2) - B(2))) * 0.41 + ...
                     max(abs(A(1) - B(1)), abs(A(2) - B(2))) * 1;
         end
-
+        function check = check_valid(obj, map, point)
+            check = 1;
+            for x = round(point(1)) - 1: 1: round(point(1)) + 1
+                for y = round(point(2)) - 1: 1: round(point(2)) + 1
+                    if (x > 0 && y > 0)
+                        if map(x, y) == 1
+                            check = 0;
+                            return;
+                        end
+                    end
+                end
+            end           
+        end
     end
 end
